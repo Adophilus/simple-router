b9610d95dbd2d8e02c504ff71bfd6104821c39ec
Adophilus <uchenna19of@gmail.com>
2023-04-02 23:46:38 +0100

    refactored main script to make use of router

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+import Router from './router'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', () => {
+  const router = new Router([
+    {
+      name: 'numbers',
+      path: /\/numbers\/\d/,
+      render() {
+        return 'home'
+      }
+    }
+  ])
+  const triggerBtn = document.querySelector("#trigger")
+  let count = 0
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  triggerBtn?.addEventListener('click', () => {
+    router.goto(`/numbers/${++count}`)
+  })
+
+  router.onChange(() => {
+    console.log(router.outlet)
+  })
+})
