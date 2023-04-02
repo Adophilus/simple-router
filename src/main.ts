b88e4dfbe9ab1062526bb5cb9ff8559eae7e8a4e
Adophilus <uchenna19of@gmail.com>
2023-04-03 00:08:57 +0100

    implemented additional routes

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,42 @@ import Router from './router'
 document.addEventListener('DOMContentLoaded', () => {
   const router = new Router([
     {
+      name: 'home',
+      path: '/',
+      async enter() {
+        await import("./examples/components/app.component");
+      },
+      async render() {
+        return '<wc-app></wc-app>'
+      }
+    },
+    {
       name: 'numbers',
       path: /\/numbers\/\d/,
-      render() {
-        return 'home'
+      async render() {
+        return 'the current number is actually some random number!'
       }
     }
   ])
-  const triggerBtn = document.querySelector("#trigger")
+
+  const homeBtn = document.querySelector("#home")
+  const numbersBtn = document.querySelector("#numbers")
+  const outletContainer = document.querySelector("#outlet")
   let count = 0
 
-  triggerBtn?.addEventListener('click', () => {
+  homeBtn?.addEventListener('click', () => {
+    router.goto('/')
+  })
+
+  numbersBtn?.addEventListener('click', () => {
     router.goto(`/numbers/${++count}`)
   })
 
-  router.onChange(() => {
-    console.log(router.outlet)
+  router.onChange(async () => {
+    const outlet = await router.outlet()
+    if (outletContainer)
+      outletContainer.innerHTML = outlet
   })
+
+  router.init()
 })
